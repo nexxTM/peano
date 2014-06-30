1bd3e41f5c715f88855d4720905308d81767a08f
Marius Loewe <mlo@informatik.uni-kiel.de>
2014-06-30 06:06:00 +0200

    Initial Tests
    Basic Quickcheck setup and some simple tests.

--- tests/Main.hs
+++ tests/Main.hs
@@ -5,12 +5,32 @@ import Peano
 import Test.Framework as TF (defaultMain, testGroup, Test)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 
+import Test.QuickCheck (Property, Arbitrary, arbitrary, shrink, choose, (==>))
+
+import Data.Functor ((<$>))
+
+instance Arbitrary Nat where
+    arbitrary = toEnum <$> (choose (0,42))
+    shrink (Succ n) = [n]
+    shrink Zero = []
+
 main :: IO ()
 main = defaultMain tests
 
 tests :: [TF.Test]
 tests = [
         testGroup "QuickCheck Peano" [
-
+            testProperty "Reflexiv" $ peanoReflexive,
+            testProperty "Symmetric" $ peanoSymmetric,
+            testProperty "Transitive" $ peanoTransitive
         ]
     ]
+
+peanoReflexive :: Nat -> Bool
+peanoReflexive n = n == n
+
+peanoSymmetric :: Nat -> Nat -> Property
+peanoSymmetric n m = n == m ==> m == n
+
+peanoTransitive :: Nat -> Nat -> Nat -> Property
+peanoTransitive n m o = n == m && m == o ==> n == o
